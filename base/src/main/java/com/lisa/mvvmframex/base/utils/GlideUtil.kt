88f3ea28d947b572f250e3938095522c4df0b39a
renliqian <3238615193@qq.com>
2020-07-10 17:54:41 +0800

    更新库

--- base/src/main/java/com/lisa/mvvmframex/base/utils/GlideUtil.kt
+++ base/src/main/java/com/lisa/mvvmframex/base/utils/GlideUtil.kt
@@ -1,12 +1,18 @@
 package com.lisa.mvvmframex.base.utils
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.lisa.mvvmframex.base.R
+import kotlin.math.roundToInt
 
 /**
  * @Description: 图片加载Glide工具类
@@ -29,7 +35,7 @@ object GlideUtil {
         iv: ImageView,
         @DrawableRes placeholder: Int
     ) {
-        configGlide(Glide.with(context), path, iv, placeholder)
+        loadSpecifyHWImage(Glide.with(context), path, iv, placeholder)
     }
 
     /**
@@ -47,14 +53,14 @@ object GlideUtil {
         iv: ImageView,
         @DrawableRes placeholder: Int
     ) {
-        configGlide(Glide.with(context), path, iv, placeholder)
+        loadSpecifyHWImage(Glide.with(context), path, iv, placeholder)
     }
 
     /**
-     * Glide配置
+     * 加载指定宽高的图片
      */
     @JvmStatic
-    private fun configGlide(
+    private fun loadSpecifyHWImage(
         requestManager: RequestManager,
         path: Any?,
         iv: ImageView,
@@ -62,7 +68,92 @@ object GlideUtil {
     ) {
         requestManager.load(path)
             .centerCrop()
-            .placeholder(if (placeholder == 0) R.drawable.default_image_placeholder else placeholder) //                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .placeholder(if (placeholder == 0) R.drawable.default_image_placeholder else placeholder)
+            .into(iv)
+    }
+
+    /**
+     * 设置Fragment自适应高度图片
+     *
+     * @param context
+     * @param path Any类型可以适配Uri
+     * @param iv
+     * @param placeholder
+     */
+    @JvmStatic
+    fun setFragmentAdjustImage(
+        context: Fragment,
+        path: Any?,
+        iv: ImageView,
+        @DrawableRes placeholder: Int
+    ) {
+        loadAdjustHeightImage(Glide.with(context), path, iv, placeholder)
+    }
+
+
+    /**
+     * 设置Activity自适应高度图片
+     *
+     * @param context
+     * @param path Any类型可以适配Uri
+     * @param iv
+     * @param placeholder
+     */
+    @JvmStatic
+    fun setActivityAdjustImage(
+        context: Activity,
+        path: Any?,
+        iv: ImageView,
+        @DrawableRes placeholder: Int
+    ) {
+        loadAdjustHeightImage(Glide.with(context), path, iv, placeholder)
+    }
+
+    /**
+     * 加载固定宽度,自适应高度的图片
+     */
+    @JvmStatic
+    private fun loadAdjustHeightImage(
+        requestManager: RequestManager,
+        path: Any?,
+        iv: ImageView,
+        @DrawableRes placeholder: Int
+    ) {
+        requestManager
+            .load(path)
+            .skipMemoryCache(true)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    @Nullable e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: com.bumptech.glide.load.DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    //判断imageView的填充方式,如果不是fitXY的填充方式 设置其填充方式
+                    if (iv.scaleType !== ImageView.ScaleType.FIT_XY) {
+                        iv.scaleType = ImageView.ScaleType.FIT_XY
+                    }
+                    //进行宽度为match_parent时的适应imageView的高度计算
+                    val params = iv.layoutParams
+                    val vw = iv.width - iv.paddingLeft - iv.paddingRight
+                    val scale = vw.toFloat() / resource.intrinsicWidth.toFloat()
+                    val vh = (resource.intrinsicHeight * scale).roundToInt()
+                    params.height = vh + iv.paddingTop + iv.paddingBottom
+                    iv.layoutParams = params
+                    return false
+                }
+            })
+            .placeholder(if (placeholder == 0) R.drawable.default_image_placeholder else placeholder)
             .into(iv)
     }
 }
